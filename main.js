6f1333eb93ea1f5fa22bb7f451b24bbb092a6ab3
Fercus <fercus@fercus.zusammen>
2018-03-27 10:20:51 +0200

    html with bootstrap and css

---
+++ main.js
@@ -0,0 +1,15 @@
+var usersData = data.graphql.user;
+
+var name = data.username;
+var followers = data.edge_followed_by.count;
+var totalLikes;
+
+$(totalLikes).each(function(likes){
+	
+	var total = likes
+
+	});
+
+
+name = document.getElementById("name1");
+followers = document.getElementById("followers1");
